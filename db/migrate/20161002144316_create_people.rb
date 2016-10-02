dd927d9db4c0a46d197e32af64cfbe9432f3bb67
FPierre <pflauder@gmail.com>
2016-10-02 16:43:53 +0200

    Création du Scaffold Person

---
+++ db/migrate/20161002144316_create_people.rb
@@ -0,0 +1,13 @@
+class CreatePeople < ActiveRecord::Migration[5.0]
+  def change
+    create_table :people do |t|
+      t.string :first_name
+      t.string :last_name
+      t.integer :gender
+      t.date :birth_date
+      t.date :death_date
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161002143154) do
+ActiveRecord::Schema.define(version: 20161002144316) do
 
   create_table "families", force: :cascade do |t|
     t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "people", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.integer  "gender"
+    t.date     "birth_date"
+    t.date     "death_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
