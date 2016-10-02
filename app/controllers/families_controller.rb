0271b76a88091fa1953abce542b64da713305e22
FPierre <pflauder@gmail.com>
2016-10-02 20:39:52 +0200

    Suppression de l'action index et de sa View de Family et nested de Person sous Family

--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -1,10 +1,6 @@
 class FamiliesController < ApplicationController
   before_action :set_family, only: [:show, :edit, :update, :destroy]
 
-  def index
-    @families = Family.all
-  end
-
   def show
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :people, :families
 
-  root 'families#index'
+  resources :families, only: [:show, :new, :edit, :create, :update, :destroy] do
+    resources :people
+  end
+
+  root 'peoples#index'
 end
