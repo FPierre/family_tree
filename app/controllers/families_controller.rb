28b2b70f8e29df04e3fcf72a33368fb8171d58ac
FPierre <pflauder@gmail.com>
2016-10-02 20:33:33 +0200

    Ajustement de l'indentation des actions privates des controllers de Family et Person

--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -1,25 +1,20 @@
 class FamiliesController < ApplicationController
   before_action :set_family, only: [:show, :edit, :update, :destroy]
 
-  # GET /families
   def index
     @families = Family.all
   end
 
-  # GET /families/1
   def show
   end
 
-  # GET /families/new
   def new
     @family = Family.new
   end
 
-  # GET /families/1/edit
   def edit
   end
 
-  # POST /families
   def create
     @family = Family.new(family_params)
 
@@ -30,7 +25,6 @@ class FamiliesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /families/1
   def update
     if @family.update(family_params)
       redirect_to @family, notice: 'Family was successfully updated.'
@@ -39,20 +33,18 @@ class FamiliesController < ApplicationController
     end
   end
 
-  # DELETE /families/1
   def destroy
     @family.destroy
     redirect_to families_url, notice: 'Family was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_family
-      @family = Family.find(params[:id])
-    end
 
-    # Only allow a trusted parameter "white list" through.
-    def family_params
-      params.require(:family).permit(:name)
-    end
+  def set_family
+    @family = Family.find(params[:id])
+  end
+
+  def family_params
+    params.require(:family).permit(:name)
+  end
 end
--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,25 +1,20 @@
 class PeopleController < ApplicationController
   before_action :set_person, only: [:show, :edit, :update, :destroy]
 
-  # GET /people
   def index
     @people = Person.all
   end
 
-  # GET /people/1
   def show
   end
 
-  # GET /people/new
   def new
     @person = Person.new
   end
 
-  # GET /people/1/edit
   def edit
   end
 
-  # POST /people
   def create
     @person = Person.new(person_params)
 
@@ -30,7 +25,6 @@ class PeopleController < ApplicationController
     end
   end
 
-  # PATCH/PUT /people/1
   def update
     if @person.update(person_params)
       redirect_to @person, notice: 'Person was successfully updated.'
@@ -39,20 +33,18 @@ class PeopleController < ApplicationController
     end
   end
 
-  # DELETE /people/1
   def destroy
     @person.destroy
     redirect_to people_url, notice: 'Person was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_person
-      @person = Person.find(params[:id])
-    end
 
-    # Only allow a trusted parameter "white list" through.
-    def person_params
-      params.require(:person).permit(:first_name, :gender, :birth_date, :death_date)
-    end
+  def set_person
+    @person = Person.find(params[:id])
+  end
+
+  def person_params
+    params.require(:person).permit(:first_name, :gender, :birth_date, :death_date)
+  end
 end
